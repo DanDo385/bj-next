d3534fd1a63810040b26a6dc0986536c2c6673a6
DanDo <DanDo385@yahoo.com>
2025-02-15 19:54:12 -0500

    fixing useContext -> shift to python

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,28 +1,21 @@
 // pages/_app.js
-// This is the main application wrapper component that provides global state and styling
+// This is the main application wrapper component that provides styling
 
-import GameProvider from "../context/GameProvider";
 import "../styles/globals.css";
-import CardCount from "../components/CardCount";
 
 /**
  * MyApp component that wraps the entire application
  * @param {Object} props - Component props
  * @param {React.Component} props.Component - The active page component
  * @param {Object} props.pageProps - Props passed to the page component
- * @returns {JSX.Element} Wrapped application with game context and layout
+ * @returns {JSX.Element} Wrapped application with layout
  */
 function MyApp({ Component, pageProps }) {
   return (
-    <GameProvider>
-      {/* Main container with minimum full viewport height */}
-      <div className="relative min-h-screen">
-        {/* Card counting display component */}
-        <CardCount />
-        {/* Active page component with its props */}
-        <Component {...pageProps} />
-      </div>
-    </GameProvider>
+    <div className="relative min-h-screen">
+      {/* Active page component with its props */}
+      <Component {...pageProps} />
+    </div>
   );
 }
 
